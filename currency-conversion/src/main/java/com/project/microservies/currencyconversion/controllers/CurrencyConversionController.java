278c75219bc786f5f4c0d1a1b3387f44d1a3f39e
Prakhar Srivastava <softzon21@gmail.com>
2020-04-12 01:37:25 +0530

    Added spring sleuth for distributed tracing

--- currency-conversion/src/main/java/com/project/microservies/currencyconversion/controllers/CurrencyConversionController.java
+++ currency-conversion/src/main/java/com/project/microservies/currencyconversion/controllers/CurrencyConversionController.java
@@ -2,6 +2,8 @@ package com.project.microservies.currencyconversion.controllers;
 
 import com.project.microservies.currencyconversion.models.ConvertedCurrency;
 import com.project.microservies.currencyconversion.proxy.CurrencyExchangeServiceProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CurrencyConversionController {
 
     private CurrencyExchangeServiceProxy currencyExchangeServiceProxy;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public CurrencyConversionController(CurrencyExchangeServiceProxy currencyExchangeServiceProxy) {
         this.currencyExchangeServiceProxy = currencyExchangeServiceProxy;
@@ -23,6 +26,7 @@ public class CurrencyConversionController {
         ConvertedCurrency response = currencyExchangeServiceProxy.retriveExchangeValue(from, to);
         response.setQuantity(quantity);
         response.setConvertedValue(response.getConversionMultiple() * quantity);
+        logger.info("response -> {}", response);
         return response;
     }
 }
--- zuul-api-gateway-server/src/main/java/com/projects/microservices/zuulapigatewayserver/ZuulApiGatewayServerApplication.java
+++ zuul-api-gateway-server/src/main/java/com/projects/microservices/zuulapigatewayserver/ZuulApiGatewayServerApplication.java
@@ -1,10 +1,12 @@
 package com.projects.microservices.zuulapigatewayserver;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -15,4 +17,8 @@ public class ZuulApiGatewayServerApplication {
         SpringApplication.run(ZuulApiGatewayServerApplication.class, args);
     }
 
+    @Bean
+    public Sampler defaultSampler(){
+        return Sampler.ALWAYS_SAMPLE;
+    }
 }
