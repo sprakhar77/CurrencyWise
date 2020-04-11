c013036317f0b49d909fce484f49c086ceaa8654
Prakhar Srivastava <softzon21@gmail.com>
2020-04-12 00:27:16 +0530

    Added Eureka naming server

--- currency-conversion/src/main/java/com/project/microservies/currencyconversion/CurrencyConversionApplication.java
+++ currency-conversion/src/main/java/com/project/microservies/currencyconversion/CurrencyConversionApplication.java
@@ -2,10 +2,12 @@ package com.project.microservies.currencyconversion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableDiscoveryClient
+@EnableFeignClients("com.project.microservies.currencyconversion")
 public class CurrencyConversionApplication {
 
     public static void main(String[] args) {
